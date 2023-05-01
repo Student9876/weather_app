378e9f0e510db6dac5fe58120f7eac997567343a
Student9876 <shouvik9876@gmail.com>
2023-05-01 08:15:27 +0530

    App.js edit

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post("/", function (req, res) {
                             <meta charset="UTF-8">
                             <meta http-equiv="X-UA-Compatible" content="IE=edge">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <link rel="stylesheet" href="/bootstrap-offline/bootstrap.css">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
                             <title>Result</title>
                         </head>
                         <body>
